77cd880f7e53a50bdd320186a0915abeb36694e0
kabu1204 <1036058484@qq.com>
2021-12-04 01:24:30 +0800

    act the same as ILisp now

--- src/lisparse.rs
+++ src/lisparse.rs
@@ -8,7 +8,8 @@ use colored::Colorize;
 pub enum Atomic {
     Number(i32),
     Float(f64),
-    Symbol(String)  // TODO: refactor to &str
+    Symbol(String),  // TODO: refactor to &str
+    Fun(fn(Vec<Atomic>)->Atomic)
 }
 
 #[derive(Debug, Clone)]
@@ -34,27 +35,23 @@ pub fn cvt_to_nested_expression(splited_cmd: &[String], idx: &mut usize) ->LispT
         let item = match &content[..] {
             "(" => cvt_to_nested_expression(splited_cmd, idx),
             ")" => {return LispType::List(tv);},
-            _ => if let true = is_symbol(&content){
-                    LispType::Atom(Atomic::Symbol(content.clone()))
-                }
-                else {
-                    match is_number(&content) {
+            _ => match is_number(&content) {
                         1 => LispType::Atom(Atomic::Number(content.parse::<i32>().unwrap())),
                         2 => LispType::Atom(Atomic::Float(content.parse::<f64>().unwrap())),
-                        _ => {
-                            println!("Undefined symbol:{}", content);
-                            LispType::Atom(Atomic::Number(0))
-                        }
+                        _ => LispType::Atom(Atomic::Symbol(content.clone()))
                     }
-                }
         };
         tv.push(item);
     }
     LispType::List(tv)
 }
 
-pub fn is_symbol(s: &String) ->bool {
-    true
+pub fn is_func(s: &String, env_op: &HashMap<String, fn(Vec<Atomic>)->Atomic>) ->bool {
+    return env_op.contains_key(s);
+}
+
+pub fn is_symbol(s: &String, env_symbol: &HashMap<String, Atomic>) ->bool {
+    return env_symbol.contains_key(s);
 }
 
 pub fn is_number(s: &String) ->i32 {
@@ -69,12 +66,19 @@ pub fn is_number(s: &String) ->i32 {
 
 pub fn eval(expr: &LispType, env_symbol: &mut HashMap<String, Atomic>,
                             env_op: &mut HashMap<String, fn(Vec<Atomic>)->Atomic>) -> Atomic {
-    let _if = Atomic::Symbol(String::from("if"));
-    let _def = Atomic::Symbol(String::from("define"));
     match expr {
         LispType::Atom(atom) => {
             match atom {
-                Atomic::Symbol(sym) => { return env_symbol[sym].clone(); },
+                Atomic::Symbol(sym) => {
+                    if is_func(sym, env_op) {
+                        return Atomic::Fun(env_op[sym]);
+                    } else if is_symbol(sym, env_symbol) {
+                        return env_symbol[sym].clone();
+                    } else {
+                        println!("{} {}",sym.green(),"is not defined!\n".red());
+                        return Atomic::Number(-1);
+                    }
+                },
                 _ => {  return atom.clone(); }
             };
         }
@@ -84,7 +88,8 @@ pub fn eval(expr: &LispType, env_symbol: &mut HashMap<String, Atomic>,
                 println!("{}","Empty list!".green());
                 return Atomic::Number(-1);
             }
-            match eval(&list[0], env_symbol, env_op) {
+            let result = eval(&list[0], env_symbol, env_op);
+            match result {
                 Atomic::Symbol(op_name) => {
                     match &op_name[..] {
                         "if" => {
@@ -108,19 +113,59 @@ pub fn eval(expr: &LispType, env_symbol: &mut HashMap<String, Atomic>,
                             }
                         }
                         _ => {
-                            let f = env_op[&op_name];
-                            return f(list[1..].into_iter().map(|subexpr| eval(subexpr, env_symbol, env_op)).collect());
+                            println!("{}","Undefined keywords!".red());
                         }
                     }
                 },
-                _ => { println!("{}","Undefined behavior".red()); }
-            }
-            for sub_expr in list.iter() {
-
+                Atomic::Fun(f) => {
+                    return f(list[1..].into_iter().map(|subexpr| eval(subexpr, env_symbol, env_op)).collect());
+                },
+                _ => {
+                    return result.clone();
+                }
             }
         }
     }
-    Atomic::Number(-1)
+    Atomic::Number(0)
+}
+
+pub fn init_env_symbol() ->HashMap<String, Atomic> {
+    let mut env_symbol: HashMap<String, Atomic> = HashMap::new();
+    env_symbol.insert(String::from("PI"), Atomic::Float(std::f64::consts::PI));
+    env_symbol.insert(String::from("if"), Atomic::Symbol(String::from("if")));
+    env_symbol.insert(String::from("define"), Atomic::Symbol(String::from("define")));
+    env_symbol
+}
+
+pub fn init_env_op() ->HashMap<String, fn(Vec<Atomic>)->Atomic> {
+    let mut env_op: HashMap<String, fn(Vec<Atomic>)->Atomic> = HashMap::new();
+    env_op.insert(String::from("+"), add);
+    env_op.insert(String::from("-"), minus);
+    env_op.insert(String::from("*"), mul);
+    env_op.insert(String::from("/"), div);
+    env_op.insert(String::from(">"), gt);
+    env_op.insert(String::from(">="), ge);
+    env_op.insert(String::from("<"), lt);
+    env_op.insert(String::from("<="), le);
+    env_op.insert(String::from("="), eq);
+    env_op.insert(String::from("/="), neq);
+    env_op.insert(String::from("begin"), begin);
+    env_op
+}
+
+/*********************************/
+/**** Below are env functions ****/
+/*********************************/
+
+macro_rules! get_atom_value {
+    ($atom:expr, $ret_type:ty) => {match $atom {
+        Atomic::Number(n) => n as $ret_type,
+        Atomic::Float(fp) => fp as $ret_type,
+        _ => {
+            println!("{}", "Operands should be of type i32 or f64".red());
+            -1 as $ret_type
+        }
+    }};
 }
 
 pub fn add(args: Vec<Atomic>) ->Atomic {
@@ -131,13 +176,136 @@ pub fn add(args: Vec<Atomic>) ->Atomic {
         match atom {
             Atomic::Number(n) => { res_n+=n; },
             Atomic::Float(fp) => { res_fp+=fp; flag=true; },
-            _ => {
-                println!("{}", "Operands should be of type i32 or f64".red());
-            }
+            _ => { println!("{}", "Operands should be of type i32 or f64".red()); }
         }
     }
     if flag {
         return Atomic::Float(res_fp+(res_n as f64));
     }
     Atomic::Number(res_n)
+}
+
+pub fn minus(args: Vec<Atomic>) ->Atomic {
+    match args[0] {
+        Atomic::Number(n) => {
+            return Atomic::Number(n - get_atom_value!(args[1],i32));
+        },
+        Atomic::Float(fp) => {
+            return Atomic::Float(fp - get_atom_value!(args[1],f64));
+        },
+        _ => { println!("{}", "Operands should be of type i32 or f64".red()); }
+    }
+    Atomic::Number(-1)
+}
+
+pub fn mul(args: Vec<Atomic>) ->Atomic {
+    let mut res_n = 1;
+    let mut res_fp = 1.0;
+    let mut flag = false;
+    for atom in args.iter(){
+        match atom {
+            Atomic::Number(n) => { res_n*=n; },
+            Atomic::Float(fp) => { res_fp*=fp; flag=true; },
+            _ => { println!("{}", "Operands should be of type i32 or f64".red()); }
+        }
+    }
+    if flag {
+        return Atomic::Float(res_fp+(res_n as f64));
+    }
+    Atomic::Number(res_n)
+}
+
+pub fn div(args: Vec<Atomic>) ->Atomic {
+    match args[0] {
+        Atomic::Number(n) => {
+            return Atomic::Float(n as f64 / get_atom_value!(args[1],f64));
+        },
+        Atomic::Float(fp) => {
+            return Atomic::Float(fp / get_atom_value!(args[1],f64));
+        },
+        _ => { println!("{}", "Operands should be of type i32 or f64".red()); }
+    }
+    Atomic::Number(-1)
+}
+
+pub fn gt(args: Vec<Atomic>) ->Atomic {
+    match args[0] {
+        Atomic::Number(n) => {
+            return Atomic::Number(i32::from((n as f64) > get_atom_value!(args[1],f64)));
+        },
+        Atomic::Float(fp) => {
+            return Atomic::Number(i32::from(fp > get_atom_value!(args[1],f64)));
+        },
+        _ => { println!("{}", "Operands should be of type i32 or f64".red()); }
+    }
+    Atomic::Number(0)
+}
+
+pub fn lt(args: Vec<Atomic>) ->Atomic {
+    match args[0] {
+        Atomic::Number(n) => {
+            return Atomic::Number(i32::from((n as f64) < get_atom_value!(args[1],f64)));
+        },
+        Atomic::Float(fp) => {
+            return Atomic::Number(i32::from(fp < get_atom_value!(args[1],f64)));
+        },
+        _ => { println!("{}", "Operands should be of type i32 or f64".red()); }
+    }
+    Atomic::Number(-1)
+}
+
+pub fn ge(args: Vec<Atomic>) ->Atomic {
+    match args[0] {
+        Atomic::Number(n) => {
+            return Atomic::Number(i32::from((n as f64) >= get_atom_value!(args[1],f64)));
+        },
+        Atomic::Float(fp) => {
+            return Atomic::Number(i32::from(fp >= get_atom_value!(args[1],f64)));
+        },
+        _ => { println!("{}", "Operands should be of type i32 or f64".red()); }
+    }
+    Atomic::Number(-1)
+}
+
+pub fn le(args: Vec<Atomic>) ->Atomic {
+    match args[0] {
+        Atomic::Number(n) => {
+            return Atomic::Number(i32::from((n as f64) <= get_atom_value!(args[1],f64)));
+        },
+        Atomic::Float(fp) => {
+            return Atomic::Number(i32::from(fp <= get_atom_value!(args[1],f64)));
+        },
+        _ => { println!("{}", "Operands should be of type i32 or f64".red()); }
+    }
+    Atomic::Number(-1)
+}
+
+pub fn eq(args: Vec<Atomic>) ->Atomic {
+    match args[0] {
+        Atomic::Number(n) => {
+            return Atomic::Number(i32::from((n as f64) == get_atom_value!(args[1],f64)));
+        },
+        Atomic::Float(fp) => {
+            return Atomic::Number(i32::from(fp == get_atom_value!(args[1],f64)));
+        },
+        _ => { println!("{}", "Operands should be of type i32 or f64".red()); }
+    }
+    Atomic::Number(-1)
+}
+
+pub fn neq(args: Vec<Atomic>) ->Atomic {
+    match args[0] {
+        Atomic::Number(n) => {
+            return Atomic::Number(i32::from((n as f64) != get_atom_value!(args[1],f64)));
+        },
+        Atomic::Float(fp) => {
+            return Atomic::Number(i32::from(fp != get_atom_value!(args[1],f64)));
+        },
+        _ => { println!("{}", "Operands should be of type i32 or f64".red()); }
+    }
+    Atomic::Number(-1)
+}
+
+pub fn begin(args: Vec<Atomic>) ->Atomic {
+    return args.last().unwrap().clone();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
-use std::collections::HashMap;
 use colored::Colorize;
 use dialoguer::Input;
-use crate::lisparse::cvt_to_nested_expression;
 
 mod lisparse;
 
 fn main() {
     println!("{}\n{}","This is a Lisp interpreter with Rust runtime.".green(),"Press C-c to exit.".green());
     let mut _buf = String::new();
-    let mut Env_Symbols:HashMap<String, lisparse::Atomic> = HashMap::new();
-    Env_Symbols.insert("PI".to_string(),lisparse::Atomic::Float(std::f64::consts::PI));
+    let mut env_symbols = lisparse::init_env_symbol();
+    let mut env_op = lisparse::init_env_op();
     loop{
         // Use RustyLine instead
         _buf = Input::new().with_prompt("> ").interact_text().unwrap();
         println!("{:?}", _buf);
         let v = lisparse::split_cmd_to_vec(&_buf);
-        for s in v.iter() {
-            println!("{}",s);
-        }
-        println!("{:?}",cvt_to_nested_expression(&v[..],&mut(0 as usize)));
+        let expr = lisparse::cvt_to_nested_expression(&v[..],&mut(0 as usize));
+        let result = lisparse::eval(&expr, &mut env_symbols, &mut env_op);
+        println!("{:?}",result);
     }
 }
